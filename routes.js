28cbb91c9bbe471cdf8c6d1f1b31794b37e99802
Kelly Born <kellyaborn@gmail.com>
2020-06-15 15:51:57 -0400

    added veg and mealprep top rating

--- routes.js
+++ routes.js
@@ -28,6 +28,22 @@ routes.get("/meatfeaturedreviews", (request, response) => {
     });
 });
 
+//GET veggie featured reviews
+routes.get("/vegfeaturedreviews", (request, response) => {
+    pool.query("SELECT * FROM reviews JOIN subs ON reviews.subscription_id = subs.sub_id WHERE sub_type = 'veg'").then((result) => {
+        console.log(result.rows)
+        response.json(result.rows);
+    });
+});
+
+//GET meal prep featured reviews
+routes.get("/mealprepfeaturedreviews", (request, response) => {
+    pool.query("SELECT * FROM reviews JOIN subs ON reviews.subscription_id = subs.sub_id WHERE sub_type = 'mealprep'").then((result) => {
+        console.log(result.rows)
+        response.json(result.rows);
+    });
+});
+
 
 routes.post("/home", (req, res) => {
     pool.query("INSERT INTO reviews(review, rating, subscription_id, review_title, user_cost ) VALUES ($1::text, $2::int, $3::int, $4::text, $5::money )",
