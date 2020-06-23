8d292861cd4bf54b0f0c4fab1c8f7059b0f0b753
isaac <gilman.isaac@gmail.com>
2020-06-23 14:38:08 -0400

    db stuff

--- connection.js
+++ connection.js
@@ -1,10 +1,13 @@
-const { Pool } = require("pg");
-const credentials = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "SubscriptionsDB",
-    ssl: false
+const { Pool } = require('pg');
+try {
+    // When not running via Heroku, this will load the .env file.
+    require('dotenv').config();
+} catch (e) {
+    // When running with Heroku, dotenv doesn't need to be available.
+}
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+    connectionString: connectionString,
+    ssl: !connectionString.includes('localhost')
 });
-module.exports = credentials;
+module.exports = pool;
